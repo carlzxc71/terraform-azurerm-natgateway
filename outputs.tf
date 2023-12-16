98776acc64e3e2584be3f69ab188994196c63aa4
Carl Lindberg <carllindberg@Carls-MacBook-Pro.local>
2023-12-16 10:35:26 +0100

    added output of the public ip address for the deployment

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,9 @@ output "name" {
   value       = azurerm_nat_gateway.this.name
   description = "The name of the nat gateway"
 }
+
+output "pip_address" {
+  value       = azurerm_public_ip.this.ip_address
+  description = "The address of the public ip"
+  
+}
